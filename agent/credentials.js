e810faf57c5b53abb65d2c8396addbd450e0fce0
Andy Awidarto <andy.awidarto@gmail.com>
2013-06-13 09:49:47 +0700

    change access credentials

--- agent/credentials.js
+++ agent/credentials.js
@@ -1,8 +1,8 @@
 var credentials = {
-    consumer_key: 'WYYOGrEUaivAP6egCWWHgA',
-    consumer_secret: '1IpGruMiRnPWznsZx52Btbd5nmZlS5MRyIZQCk8DzUs',
-    access_token_key: '46893111-B8ZutxnBKoSFIwM9ne2VC0Hap4xrfz7Xf20Ds7guZ',
-    access_token_secret: 'pAMRzzvUJvxop31yMizkwRsfbVXAHaqFO4HP6Hk'
+    consumer_key: 'xRwQv7OKhFCQllogJSYw',
+    consumer_secret: 'MGjT0AbySJcCdCy62pb4HxfC3Sg0NJFrTkV7jTJyChk',
+    access_token_key: '46893111-feweZcnyyD7FZAVNzm4dZRKa5THvwCqy4Lzgr6oI',
+    access_token_secret: 'hqTsDkicBhsxcz4AbQJkeKYWvXu0MIf6vM9xNZY6Y'
 };
 
 module.exports = credentials;
